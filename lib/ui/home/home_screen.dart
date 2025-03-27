83b4cb5552072180b263421e78876c0ae4ac1158
Hidalgo87 <juanfgomez6@gmail.com>
2025-03-26 19:27:33 -0500

    create proyect

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'Hola',
+            style: TextStyle(color: Colors.red, fontSize: 40),
+          ),
+        ),
+      ),
+    );
+  }
+}
